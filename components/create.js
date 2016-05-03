f843ad8f4784bbc96f296023f9ec36bedee31717
Piet Geursen <pietgeursen@gmail.com>
2016-05-03 14:52:42 +1200

    Adding create form.

--- components/create.js
+++ components/create.js
@@ -6,9 +6,25 @@ const Create = {
   render ({props}) {
     return (
       <div class={prefix}>
-        <form>
-          
-        </form>
+        <div class='section'>
+          <h2>Create a new event</h2>
+          <form>
+            <label for="eventNameInput">Event Name</label>
+            <input class='u-full-width' type="text" placeholder="My sweet event" id="eventNameInput" />
+            <div class='row'>
+              <div class='six columns'>
+                <label for="eventDate">Date</label>
+                <input class='u-full-width' type='date' id="eventDate" />
+              </div>
+              <div class='six columns'>
+                <label for="eventTimeInput">Time</label>
+                <input class='u-full-width' type='time' id="eventTimeInput" />
+              </div>
+            </div>
+            <label for="eventDescriptionInput">Event Description</label>
+            <textarea class='u-full-width' placeholder="Fun times" id="eventDescriptionInput" ></textarea>
+          </form>
+        </div>
       </div>
     ) 
   }
