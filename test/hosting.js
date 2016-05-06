9760025e321577b48fb3701adef47c39f41fb030
Piet Geursen <pietgeursen@gmail.com>
2016-05-06 15:07:44 +1200

    Refactor to remove gossiping and replication.

--- test/hosting.js
+++ test/hosting.js
@@ -17,35 +17,28 @@ function follow (id) {
 }
 
 test('find gets all messages by all authors', function(t) {
+  var pietKey = ssbKeys.generate()
+  var katieKey = ssbKeys.generate()
+  var sbot = createSbot({temp:'piet', keys: ssbKeys.generate()})
+  var katie = sbot.createFeed(katieKey)
+  var piet = sbot.createFeed(pietKey)
 
-  var piet = createSbot({temp:'piet', keys: ssbKeys.generate()})
-  var katie = createSbot({temp:'katie', keys: ssbKeys.generate()})
 
-  katie.gossip.add(piet.getAddress())
-  piet.gossip.add(katie.getAddress())
-
-  piet.publish(follow(katie.id), function(err, data) {
+  piet.add(follow(katie.id), function(err, data) {
   })
-  katie.publish(follow(piet.id), function(err, data) {
+  katie.add(follow(piet.id), function(err, data) {
   })
 
-  piet.events.create(validEvent,function(err, data) {})
-  katie.events.create(validEvent,function(err, data) {})
+  piet.add(validEvent,function(err, data) {})
+  katie.add(validEvent,function(err, data) {})
 
-  pull(piet.friends.createFriendStream({live: true}), pull.take(2), pull.collect(function(err, friends) {
-    t.equal(friends.find(function(friend) {
-     return friend === katie.id 
-    }),katie.id ,'friends stream gets katie as friend')
-  }))
-
-  pull(piet.events.find(), pull.take(2), pull.collect(function(err, events) {
+  pull(sbot.events.find(), pull.take(2), pull.collect(function(err, events) {
     t.equal(events[0].value.author, piet.id, 'message authored by piet')
     t.equal(events[1].value.author, katie.id, 'message authored by katie')
     t.equal(events.length, 2, 'got both events') 
 
     t.end()
-    piet.close()
-    katie.close()
+    sbot.close()
   }))
   
 })
