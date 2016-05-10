ccdfb89fe104b6444f469af337c7412634acbf17
Piet Geursen <pietgeursen@gmail.com>
2016-05-10 15:30:55 +1200

    Add test to filter rsvps by type vote.

--- test/rsvp.js
+++ test/rsvp.js
@@ -25,3 +25,24 @@ test('can get all rsvps on an event', function(t) {
     })
   })
 })
+test('can get all rsvps on an event and filter by type vote', function(t) {
+  var pietKey = ssbKeys.generate()
+  var sbot = createSbot({temp:'piety', keys: pietKey})
+
+  sbot.publish(validEvent,function(err, event) {
+    var id = event.key 
+    sbot.publish(schema.vote(id, 1),function(err, vote) {
+      sbot.publish(schema.post('wee',null, null, id), function(err, comment) {
+
+        pull(sbot.events.rsvpsOnEvent(id, {live: false}), pull.collect(function(err, data) {
+          t.equal(data.length, 1, 'one link references event')
+          t.deepEqual(data[0], vote.value)
+          sbot.close()
+          t.end()
+        }))
+      })
+    })
+  })
+})
+
+
