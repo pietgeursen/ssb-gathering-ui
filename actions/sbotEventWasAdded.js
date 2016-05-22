d0d063862ba8e0da2b4dfb59cd848365685f25d8
Piet Geursen <pietgeursen@gmail.com>
2016-05-22 17:57:58 +1200

    Add and pass test for event action.

--- actions/sbotEventWasAdded.js
+++ actions/sbotEventWasAdded.js
@@ -1,11 +1,12 @@
 import t from 'tcomb'
 import Event from '../models/event'
+import Model from '../models/model'
 
 const SbotEventAdded = t.struct({payload: Event}, 'sbotEventWasAdded')
 SbotEventAdded.prototype.update = function(model) {
-  return {model: { ...model,
-    events: model.events.concat([this.payload])
-  }}
+	return {model: Model.update(model, 
+		{events: {$push: [this.payload]}}	
+	)}
 }
 
 export default SbotEventAdded
