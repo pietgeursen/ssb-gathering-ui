7a45151b9e05e716dc67a7704d1d7cdccd8a8307
Piet Geursen <pietgeursen@gmail.com>
2016-05-26 19:55:04 +1200

    Remove console log.

--- streams/sbotGatheringWasAdded.js
+++ streams/sbotGatheringWasAdded.js
@@ -5,9 +5,7 @@ function sbotFutureGatheringWasAdded(client){
   return pull(
     client.findFutureGatherings(),
     pull.map((gathering) => {
-      console.log('gathering: ', gathering);
       var action = SbotGatheringWasAddedAction(gathering)
-      console.log('action: ',action);
       return action
     })
   )
